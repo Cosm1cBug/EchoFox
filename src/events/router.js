15fc0eab46f8dbadb595a8772e9352fa42294715
COSMICBUG <54317137+Cosm1cBug@users.noreply.github.com>
2026-06-05 14:09:55 +0530

    events route fixed

--- src/events/router.js
+++ src/events/router.js
@@ -14,22 +14,32 @@
  */
 const { EventEmitter } = require('node:events');
 
-const handleMessage        = require('./messages.upsert');
-const onGroupsUpdate       = require('./groups.update');
-const onGroupParticipants  = require('./group-participants.update');
-const onContactsUpsert     = require('./contacts.upsert');
-const onCall               = require('./call');
-const onMessagesUpdate     = require('./messages.update');
-const onMessagesDelete     = require('./messages.delete');
-const onMessageReaction    = require('./messages.reaction');
-const onMessageReceipt     = require('./message-receipt.update');
-const onConnectionUpdate   = require('./connection.update');
-const onNewsletterUpsert   = require('./newsletter.upsert');
-const onNewsletterReaction = require('./newsletter.reaction');
-const onNewsletterView     = require('./newsletter.view');
-const onBlocklistSet       = require('./blocklist.set');
-const onBlocklistUpdate    = require('./blocklist.update');
-const onLidMappingUpdate   = require('./lid-mapping.update');
+const handleMessage              = require('./messages.upsert');
+const onGroupsUpdate             = require('./groups.update');
+const onGroupParticipants        = require('./group-participants.update');
+const onContactsUpsert           = require('./contacts.upsert');
+const onCall                     = require('./call');
+const onMessagesUpdate           = require('./messages.update');
+const onMessagesDelete           = require('./messages.delete');
+const onMessageReaction          = require('./messages.reaction');
+const onMessageReceipt           = require('./message-receipt.update');
+const onConnectionUpdate         = require('./connection.update');
+const onNewsletterUpsert         = require('./newsletter.upsert');
+const onNewsletterReaction       = require('./newsletter.reaction');
+const onNewsletterView           = require('./newsletter.view');
+const onNewsletterSettingsUpdate = require('./newsletter-settings.update');
+const onBlocklistSet             = require('./blocklist.set');
+const onBlocklistUpdate          = require('./blocklist.update');
+const onLidMappingUpdate         = require('./lid-mapping.update');
+const onMessagingHistorySet      = require('./messaging-history.set');
+const onMessagingHistoryStatus   = require('./messaging-history.status');
+const onChatsUpsert              = require('./chats.upsert');
+const onChatsUpdate              = require('./chats.update');
+const onChatsDelete              = require('./chats.delete');
+const onPresenceUpdate           = require('./presence.update');
+const onLabelsAssociation        = require('./labels.association');
+const onLabelsEdit               = require('./labels.edit');
+const onMessageCappingUpdate     = require('./message-capping.update');
 
 const bus = new EventEmitter();
 bus.setMaxListeners(50);
@@ -46,9 +56,19 @@ bus.on('connection.update',         (p) => Promise.resolve(onConnectionUpdate(p)
 bus.on('newsletter.upsert',         (p) => Promise.resolve(onNewsletterUpsert(p)).catch(() => {}));
 bus.on('newsletter.reaction',       (p) => Promise.resolve(onNewsletterReaction(p)).catch(() => {}));
 bus.on('newsletter.view',           (p) => Promise.resolve(onNewsletterView(p)).catch(() => {}));
+bus.on('newsletter-settings.update',(p) => Promise.resolve(onNewsletterSettingsUpdate(p)).catch(() => {}));
 bus.on('blocklist.set',             (p) => Promise.resolve(onBlocklistSet(p)).catch(() => {}));
 bus.on('blocklist.update',          (p) => Promise.resolve(onBlocklistUpdate(p)).catch(() => {}));
 bus.on('lid-mapping.update',        (p) => Promise.resolve(onLidMappingUpdate(p)).catch(() => {}));
+bus.on('messaging-history.set',     (p) => Promise.resolve(onMessagingHistorySet(p)).catch(() => {}));
+bus.on('messaging-history.status',  (p) => Promise.resolve(onMessagingHistoryStatus(p)).catch(() => {}));
+bus.on('chats.upsert',              (p) => Promise.resolve(onChatsUpsert(p)).catch(() => {}));
+bus.on('chats.update',              (p) => Promise.resolve(onChatsUpdate(p)).catch(() => {}));
+bus.on('chats.delete',              (p) => Promise.resolve(onChatsDelete(p)).catch(() => {}));
+bus.on('presence.update',           (p) => Promise.resolve(onPresenceUpdate(p)).catch(() => {}));
+bus.on('labels.association',        (p) => Promise.resolve(onLabelsAssociation(p)).catch(() => {}));
+bus.on('labels.edit',               (p) => Promise.resolve(onLabelsEdit(p)).catch(() => {}));
+bus.on('message-capping.update',    (p) => Promise.resolve(onMessageCappingUpdate(p)).catch(() => {}));
 
 module.exports = {
   handleMessage,                                 // direct, called per-message for back-pressure
